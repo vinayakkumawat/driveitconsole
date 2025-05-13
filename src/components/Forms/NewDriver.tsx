dd393799e65277266ca41493627997a3fbd4c7ca
mattislub <166994172+mattislub@users.noreply.github.com>
2025-05-13 11:59:20 +0300

    Update NewDriver.tsx

--- src/components/Forms/NewDriver.tsx
+++ src/components/Forms/NewDriver.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useForm } from 'react-hook-form';
 import { z } from 'zod';
